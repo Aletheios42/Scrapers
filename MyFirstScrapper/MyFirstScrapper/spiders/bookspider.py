527b53e9bc52c31c4b7a690e85e1366f155a38b1
Aletheios42 <alejandropintosdev@gmail.com>
2024-08-11 18:25:10 +0200

    ahora toca coger datos de cada una de las paginas individuales de los libros

--- MyFirstScrapper/MyFirstScrapper/spiders/bookspider.py
+++ MyFirstScrapper/MyFirstScrapper/spiders/bookspider.py
@@ -10,13 +10,22 @@ class BookspiderSpider(scrapy.Spider):
         books = response.css("article.product_pod")
 
         for book in books:
-            yield {
-                "name": book.css("h3 a::text").get(),
-                "price": book.css(".product_price .price_color::text").get(),
-                "url": book.css("h3 a").attrib["href"],
-            }
+            relative_url = response.css("h3 a ::attr()")
 
-        next_page = response.css("li.next a::attr(href)").get()
-        if next_page:
-            next_page_url = response.urljoin(next_page)
+            if "cataloge/" in relative_url:
+                book_url = "https://books.toscrape.com" + relative_url
+            else:
+                book_url = "https://books.toscrape.com/cataloge/" + relative_url
+            yield response.follow(book_url, callback=self.parse_book)
+
+        next_page = response.css("li.next a ::attr()")
+
+        if next_page is not None:
+            if "cataloge/" in next_page:
+                next_page_url = "https://books.toscrape.com" + next_page
+            else:
+                next_page_url = (
+                    "https://books.toscrape.com" + "cataloge/" + next_page_url
+                )
+
             yield response.follow(next_page_url, callback=self.parse)
