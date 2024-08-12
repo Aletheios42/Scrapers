364612f8347866a2f68de23a20f169c3e8872c03
Aletheios42 <alejandropintosdev@gmail.com>
2024-08-12 16:51:07 +0200

    parseando la pagina individual de cada libro

--- BookScraper/BookScaper/BookScaper/spiders/__pycache__/bookspider.py
+++ BookScraper/BookScaper/BookScaper/spiders/__pycache__/bookspider.py
@@ -10,15 +10,15 @@ class BookspiderSpider(scrapy.Spider):
         books = response.css("article.product_pod")
 
         for book in books:
-            relative_url = response.css("h3 a ::attr()")
+            relative_url = response.css("h3 a ::attr(href)")
 
             if "catalogue/" in relative_url:
                 book_url = "https://books.toscrape.com" + relative_url
             else:
                 book_url = "https://books.toscrape.com/catalogue/" + relative_url
-            yield response.follow(book_url, callback=self.parse_book)
+            yield response.follow(book_url, callback=self.parse_book_page)
 
-        next_page = response.css("li.next a ::attr()")
+        next_page = response.css("li.next a ::attr(href)")
 
         if next_page is not None:
             if "catalogue/" in next_page:
@@ -29,3 +29,16 @@ class BookspiderSpider(scrapy.Spider):
                 )
 
             yield response.follow(next_page_url, callback=self.parse)
+
+    def parse_book_page(self, response):
+        table_rows = response.css("table tr")
+
+        yield {
+            "url": response.url,
+            "title": response.css(".product_main h1::text").get(),
+            "product_type": table_row[1].css("td ::text").get(),
+            "price_excl. tax": table_row[2].css("td ::text").get(),
+            "price_incl. tax": table_row[3].css("td ::text").get(),
+            "tax": table_row[4].css("td ::text").get(),
+        }
+        pass
